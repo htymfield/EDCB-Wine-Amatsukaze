eff2898717db3111c009c56ea6197b776a2815de
htymfield <htymfield@gmail.com>
2024-03-26 01:34:53 +0900

    AddTask Job

--- Amatsukaze/cron/job.sh
+++ Amatsukaze/cron/job.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 # $filename$がShift-JISで渡されるため注意。2バイト文字が入るとAmatsukazeServerが受け入れない
 
-date > /Amatsukaze/cron/queue/current.txt
+ip=192.168.11.20
+port=32768
+remote_dir="\\\\NASBI\\DockerData\\EDCB-Wine\\TV-Record"
+preset="auto"
+out_dir="\\\\NASBI\\DockerData\\EDCB-Wine\\TV-Record\\encoded"
 
-# mono /Amatsukaze/exe_files/AmatsukazeAddTask.exe \
-#    -f "$FileName$.ts" \
-#    -ip 192.168.11.20 -p 32768 \
-#    --remote-dir "\\\\NASBI\\DockerData\\EDCB-Wine\\TV-Record" \
-#    -s "auto" --no-move \
-#    -o "\\\\NASBI\\DockerData\\EDCB-Wine\\TV-Record\\encoded" 
+
+queue_dir="/Amatsukaze/cron/queue/*"
+files=`find $queue_dir -maxdepth 0 -type f -name *.ts`
+
+for file in $files
+do
+    mono /Amatsukaze/exe_files/AmatsukazeAddTask.exe \
+        -f "$file" \
+        -ip $ip -p $port \
+        --remote-dir $remote_dir \
+        -s $preset --no-move \
+        -o $out_dir >> /Amatsukaze/cron/queue/current.txt
+    rm $file
+done
